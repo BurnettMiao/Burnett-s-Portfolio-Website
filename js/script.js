94594fa05b61c7248b043d1bcd73108c446cfbcf
BurnettMiao <searchfor1987@gmail.com>
2023-03-06 14:32:44 +0800

    main responsive OK

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
+const showMobileMenu = document.getElementById('mobile-menu');
 
 btn.addEventListener('click', navToggle);
 
 function navToggle() {
   btn.classList.toggle('open');
+  overlay.classList.toggle('overlay-show');
+  document.body.classList.toggle('stop-scrolling');
+  showMobileMenu.classList.toggle('show-mobile-menu');
 }
